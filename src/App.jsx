e7e1e0b8fcda86a7b8cf8d285f13e3331195f4ba
Fortune Ellah <fortuneellah@Admins-MacBook-Pro.local>
2023-07-25 18:05:57 +0100

    Hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,102 @@ import { BsFillMoonStarsFill} from 'react-icons/bs'
 import { useState } from 'react';
 
 function App() {
-
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
   return (
     <>
     <div className={darkMode ? 'dark' : ""} >
        <div className=' min-h-fit bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
           <nav className='py-10 flex justify-between'>
-              <h1 className='text-xl font-burtons dark:text-white'>CHIKA_CODES~</h1>
-              <ul className='flex items-center'>
-              {/* <a href="/" className="hover:decoration-teal-600"><li className='cursor-pointer text-md mr-10 dark:text-white'>Home</li></a>
-                  <li className='cursor-pointer text-md mr-10 dark:text-white'>About</li>
-                  <li className='cursor-pointer text-md mr-10 dark:text-white'>Services</li>
-                  <li className='cursor-pointer text-md mr-10 dark:text-white'>Projects</li>
-                  <li className='cursor-pointer text-md mr-10 dark:text-white'>Blog</li>
-                  <li className='cursor-pointer text-md mr-20 dark:text-white'>Contact</li> */}
+              <h1 className='text-md font-burtons dark:text-white'>CHIKA_CODES~</h1>
+              {/* <ul className='flex items-center'> */}
+                <div className="flex items-center justify-between">
+                    <section className="MOBILE-MENU flex lg:hidden">
+                      <div
+                        className="HAMBURGER-ICON space-y-2"
+                        onClick={() => setIsNavOpen((prev) => !prev)}
+                      >
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                      </div>
 
-                  <li>
-                      <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl dark:text-white' />
-                  </li>
+                      <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                        <div
+                          className="absolute top-0 right-0 px-8 py-8"
+                          onClick={() => setIsNavOpen(false)}
+                        >
+                          <svg
+                            className="h-8 w-8 text-gray-600"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <line x1="18" y1="6" x2="6" y2="18" />
+                            <line x1="6" y1="6" x2="18" y2="18" />
+                          </svg>
+                        </div>
+                        <ul className="flex flex-col items-center justify-between min-h-[250px] ">
+                          <li className="border-b border-gray-400 my-8 uppercase ">
+                            <a href="/about">About</a>
+                          </li>
+                          <li className="border-b border-gray-400 my-8 uppercase ">
+                            <a href="/portfolio">Portfolio</a>
+                          </li>
+                          <li className="border-b border-gray-400 my-8 uppercase ">
+                            <a href="/contact">Contact</a>
+                          </li>
+                          <li>
+                            <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl mb-6' />
+                          </li>
+                          <a href='https://docs.google.com/document/d/1L2CjDej60KV6Nkh6xhbE1gSpPPQX02l2X9QPJwFsvE4/edit' className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-2'>
+                            <li>Resume</li>
+                          </a>
+                        </ul>
+                      </div>
+                    </section>
+
+                    <ul className="DESKTOP-MENU hidden space-x-8 lg:flex dark:text-white">
+                      <li className="mt-2">
+                        <a href="/about">About</a>
+                      </li>
+                      <li className="mt-2">
+                        <a href="/portfolio">Portfolio</a>
+                      </li>
+                      <li className="mt-2">
+                        <a href="/contact">Contact</a>
+                      </li>
+                      <li className="mt-2">
+                        <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl dark:text-white' />
+                      </li>
+                      <a href='https://docs.google.com/document/d/1L2CjDej60KV6Nkh6xhbE1gSpPPQX02l2X9QPJwFsvE4/edit' className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>
+                        <li>Resume</li>
+                      </a>
+                    </ul>
                 
-                  
-                  <a href='#' className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'><li>Resume</li></a>
-              </ul>
+                  <style>{`
+                  .hideMenuNav {
+                    display: none;
+                  }
+                  .showMenuNav {
+                    display: block;
+                    position: absolute;
+                    width: 100%;
+                    height: 100vh;
+                    top: 0;
+                    left: 0;
+                    background: white;
+                    z-index: 10;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-evenly;
+                    align-items: center;
+                  }
+                `}</style>
+                </div>
           </nav>
         </div>
    
