c1c330fd25c76b70d44cce24b80b0a104b0929fc
Fortune Ellah <fortuneellah@Admins-MacBook-Pro.local>
2023-07-29 15:12:36 +0100

    padding fix

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -6,11 +6,11 @@ const Services = () => {
   return (
     <div className='text-center p-10 dark:bg-gray-900'>
         <h3 className='text-3xl py-1 text-teal-600 lg:pt-20'>About Me</h3>
-        <p className='text-md p-6 text-justify leading-8 text-gray-800 dark:text-white'>
+        <p className='text-md p-2 text-justify leading-8 text-gray-800 dark:text-white'>
         Hello! I'm Fortune Ellah, a passionate and creative <span className=' text-teal-500'>Frontend Developer</span> with two years of hands-on experience in crafting delightful web experiences. My journey in the world of web development started with a fascination for the ever-evolving digital landscape, and since then, I've been on an exciting mission to bring ideas to life through HTML, CSS, JavaScript, and modern frameworks like <span className=' text-teal-500'>React </span> and <span className=' text-teal-500'>TailwindCSS.</span> As a frontend developer, I thrive on turning designs into clean, intuitive, and user-friendly interfaces. With a keen eye for detail and a passion for responsive design practices, I take pride in ensuring that every website I create provides an exceptional user experience across various devices and screen sizes.
          My approach to development is grounded in the Agile methodology, where collaboration, adaptability, and iterative improvements play a pivotal role in delivering top-notch web solutions. I value effective communication with team members and stakeholders, as it fosters a creative and productive work environment.
         </p>
-        <p className='text-md p-6 text-justify leading-8 text-gray-800 dark:text-white'>
+        <p className='text-md p-2 text-justify leading-8 text-gray-800 dark:text-white'>
         In my journey as a frontend developer, I have amassed a portfolio of diverse projects, ranging from dynamic web applications to interactive landing pages. Each project has honed my skills, deepened my understanding of industry best practices, and nurtured my love for the art of web development. When I'm not immersed in coding, you can find me exploring the latest trends and emerging technologies in the frontend development realm. I am excited about the endless possibilities that lie ahead, and I'm looking forward to collaborating with passionate individuals and innovative teams to build the next generation of exceptional web experiences.
         </p>
         <div className='gap-20 justify-center  lg:grid lg:grid-cols-2  dark:text-white'>
